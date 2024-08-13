4e5dc003760aa3cb5ada2e2c143aef78a185da8a
Mirsaid <mirsaidnajmiddinov298@gmail.com>
2024-08-13 13:38:07 +0500

    Initial commit

---
+++ src/test/java/org/example/bootproject/BootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
